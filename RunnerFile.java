b7ab2ecbc157db4ee93617724bda62709e2740b1
sudarshanr10 <sudarshan86.ramesh@gmail.com>
2022-03-16 22:08:05 -0400

    Runner and interface for printing perfect square functionality created

--- RunnerFile.java
+++ RunnerFile.java
@@ -1,5 +1,16 @@
+interface pSquare{
+	public boolean square(int number);
+}
+
 public class RunnerFile{
 	public static void main(String[]args){
-
+		pSquare p = (int number) -> {
+			if(Math.sqrt(number) % 1 == 0)
+			{
+				return true;
+			}
+			return false;
+		};
+		System.out.println("Is 49 a perfect square?"+p.square(50));
 	}
 }
